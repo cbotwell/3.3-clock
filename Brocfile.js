b5ca5f248e21213351db7ae04c2ebfbca14a53d2
Corbin Otwell <cbotwell@gmail.com>
2015-07-15 13:49:26 -0500

    added include path for neat

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,8 @@ var sass = require('broccoli-sass');
 var includePaths = [
     'assets/scss',
     'bower_components/reset-css',
-    'bower_components/bourbon/app/assets/stylesheets'
+    'bower_components/bourbon/app/assets/stylesheets',
+    'bower_components/neat/app/assets/stylesheets'
 ];
 
 var compiledCss = sass (includePaths, 'app.scss', 'app.css');
